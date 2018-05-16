16b89799e3523b15d2ca43f8e63754035867d94e
Pomogalov V.A <v.pomogalov@ftc.ru>
2018-05-16 14:27:17 +0300

    tested ok

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const BooseToken = artifacts.require('./BooseToken.sol');
+
+module.exports = (deployer) => {
+    //http://www.onlineconversion.com/unix_time.htm
+    var owner = "0xbA22878a8E5f411D9C0F42eE34aC4B1A3950763D";
+    deployer.deploy(BooseToken, owner);
+};
